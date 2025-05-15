3208b2ca1f461e4d0094ec832b91922adee9b52d
jelbob <jalal.amer.2011@gmail.com>
2025-05-15 00:34:11 -0700

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
 const SUPABASE_URL = "https://andfbrotirkajfbzseye.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFuZGZicm90aXJrYWpmYnpzZXllIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUzMTg3MjcsImV4cCI6MjA2MDg5NDcyN30.-hxJtHWeKdwpNCW_k83MqunqO8xSATtoUiCNIDTu_l8";
+const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFuZGZicm90aXJrYWpmYnpzZXllIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUzMTg3MjcsImV4cCI6MjA2MDg5NDcyN30.-hxJtHWeKdwpNCW_k83MqunqO8xSATtoUiCNIDTu...";
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
